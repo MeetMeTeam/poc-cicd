8891805938f545a9c593f2eb251ef725b931368a
winnerkypt <kanyapat.winnerkypt@mail.kmutt.ac.th>
2024-03-22 23:12:23 +0700

    add api

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"fmt"
 	"github.com/labstack/echo/v4"
+	"net/http"
 )
 
 func main() {
-	echo.New()
+	e := echo.New()
 	fmt.Println("run")
+	e.GET("/api", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
 }
